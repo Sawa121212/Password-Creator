a058e889c6ca99c66c70646c1cb1a0b424db8c89
EmployeeNikolaev <employeenikolaev@gmail.com>
2020-11-26 22:58:41 +0300

    Добавлены коментарии в некотоых местах

--- PasswordCreatorVersion2/AddKeys/AddKey.cs
+++ PasswordCreatorVersion2/AddKeys/AddKey.cs
@@ -4,98 +4,20 @@
     {
         public static bool AddNumber(string item, int index)
         {
-            var res = AddNewKey(item, index, _englishDownKeyPointStart, _englishDownKeyPointEnd, 'a');
-            /*var itemChar = item[0];     // берем элемент в char
-            if (item != "")             // проверяем, не пустой ли элемент
-            {
-                if ((int)itemChar != _numberPointEnd)   // проверяем, элемент не последняя цифра? (9)
-                {
-                    // проверяем, входит ли элемент в диапазон
-                    if ((int)itemChar < _numberPointEnd && (int)itemChar >= _numberPointStart)
-                    {
-                        ChangeKey.ToNewChar(itemChar, index);
-                    }
-                    else
-                    {
-                        ChangeKey.ToChar('0', index);
-                        //MainPassword = MainPassword.Replace(MainPassword[MainPassword.Length - 1], 'a');
-                    }
-                }
-                else
-                {
-                    return false;   // если элемент последняя цифра (9), значит с цифрами закончили
-                }
-            }
-            else
-            {
-                //MainPassword = "0"; 
-                ChangeKey.ToChar('0', index);   // если элемент пустой, то ставим стартовый элемент (0)
-            }*/
-
+            var res = AddNewKey(item, index, _englishDownKeyPointStart, _englishDownKeyPointEnd, '0');
             return res;
         }
 
         public static bool AddEnglishDownKey(string item, int index)
         {
             var res = AddNewKey(item, index, _englishDownKeyPointStart, _englishDownKeyPointEnd, 'a');
-            /*var itemChar = item[0];     // берем элемент в char
-            if (item != "")
-            {
-                if ((int)itemChar != _englishDownKeyPointEnd)
-                {
-                    if ((int)itemChar < _englishDownKeyPointEnd && (int)itemChar >= _englishDownKeyPointStart)
-                    {
-                        ChangeKey.ToNewChar(itemChar, index);
-                    }
-                    else
-                    {
-                        ChangeKey.ToChar('a', index);
-                        //MainPassword = MainPassword.Replace(MainPassword[MainPassword.Length - 1], 'a');
-                    }
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                //MainPassword = "a";
-                ChangeKey.ToChar('a', index);
-            }*/
-
             return res;
         }
 
         public static bool AddEnglishUpKey(string item, int index)
         {
             var res = AddNewKey(item, index, _englishUpKeyPointStart, _englishUpKeyPointEnd, 'A');
-            /*var itemChar = item[0];     // берем элемент в char
-            if (item != "")
-            {
-                if ((int)itemChar != _englishUpKeyPointEnd)
-                {
-                    if ((int)itemChar < _englishUpKeyPointEnd && (int)itemChar >= _englishUpKeyPointStart)
-                    {
-                        ChangeKey.ToNewChar(itemChar, index);
-                    }
-                    else
-                    {
-                        //MainPassword = MainPassword.Replace(MainPassword[MainPassword.Length - 1], 'A');
-                        ChangeKey.ToChar('A', index);
-                    }
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                ChangeKey.ToChar('A', index);
-            }*/
-
-            return true;
+            return res;
         }
 
         /// <summary>
@@ -106,11 +28,12 @@
         /// <returns></returns>
         private static bool AddNewKey(string item, int index, int indexASCIIStart, int indexASCIIEnd, char defaultChar)
         {
-            if (item != "")
+            if (item != "") // проверяем, не пустой ли элемент
             {
-                var itemChar = item[0];     // берем элемент в char
-                if ((int)itemChar != indexASCIIEnd)
+                var itemChar = item[0];             // берем элемент в char
+                if ((int)itemChar != indexASCIIEnd) // проверяем, элемент не последний ли элемент
                 {
+                    // проверяем, входит ли элемент в диапазон
                     if ((int)itemChar < indexASCIIEnd && (int)itemChar >= indexASCIIStart)
                     {
                         ChangeKey.ToNewChar(itemChar, index);
@@ -122,12 +45,12 @@
                 }
                 else
                 {
-                    return false;
+                    return false;   // если элемент последний элемент, значит с данным диапазоном закончили
                 }
             }
             else
             {
-                ChangeKey.ToChar(defaultChar, index);
+                ChangeKey.ToChar(defaultChar, index);   // если элемент пустой, то ставим стартовый элемент
             }
             return true;
         }
